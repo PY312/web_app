6ea36ccc09cfb401bedb30817e5990df9749d7fc
PY312 <83508520+PY312@users.noreply.github.com>
2021-09-23 17:00:01 +0600

    lessons 6-9

--- main/views.py
+++ main/views.py
@@ -3,7 +3,16 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    data = {
+        'title': 'Главная страница!!',
+        'values': ['some', 'hello', '123'],
+        'obj' : {
+            'car' : 'BMW',
+            'age': '18',
+            'hobby': 'programming'
+        }
+    }
+    return render(request, 'main/index.html', data)
 
 
 def about(request):
