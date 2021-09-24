b7704fbe3e8ff6f5bf6c2a79b0f11e8245bae915
PY312 <83508520+PY312@users.noreply.github.com>
2021-09-24 10:23:52 +0600

    '9-11'

--- news/views.py
+++ news/views.py
@@ -1,12 +1,29 @@
 from django.shortcuts import render, redirect
 from .models import Artiles
 from .forms import ArticlesForm
-
+from django.views.generic import DetailView, UpdateView, DeleteView
 
 # Create your views here.
 def news_home(request):
-    news = Artiles.objects.order_by('title')
+    news = Artiles.objects.order_by('-date')
     return render(request, 'news/news_home.html', {'news': news})
+
+class NewsDetailView(DetailView):
+    model = Artiles
+    template_name = 'news/detail_view.html'
+    context_object_name = 'article'
+
+class NewsUpdateView(UpdateView):
+    model = Artiles
+    template_name = 'news/create.html'
+
+    # fields = ['title', 'anons', 'full_text', 'date']
+    form_class = ArticlesForm
+
+class NewsDeleteView(DeleteView):
+    model = Artiles
+    success_url = '/news'
+    template_name = 'news/news-delete.html'
 
 
 def create(request):
